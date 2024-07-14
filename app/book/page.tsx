94d6ba57d3101c559b77736f0cd1edd37ed97e5f
Rohan <rohangawande2303@gmail.com>
2024-07-14 09:52:00 +0530

    calendly mobile responsive changes

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -21,10 +21,48 @@ const Book = () => {
           Book a meeting
         </div>
 
-        <InlineWidget url="https://calendly.com/socialpulseagencydomain/30min" />
+        <div className="flex-grow flex justify-center items-center p-4">
+          <div className="w-full md:w-3/4 lg:w-2/3" style={{ height: "80vh" }}>
+            <InlineWidget
+              url="https://calendly.com/socialpulseagencydomain/30min"
+              styles={{ height: "100%" }}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
 };
 
 export default Book;
+
+// "use client";
+
+// import { InlineWidget } from "react-calendly";
+// import Navbar from "@/components/navbar";
+
+// const Book = () => {
+//   return (
+//     <>
+//       <div className="flex flex-col w-full h-screen bg-black/[0.96] bg-grid-white/[0.02]">
+//         <Navbar
+//           scrollToWebsiteDesign={() => {}}
+//           scrollToGraphicDesign={() => {}}
+//           scrollToShopifyStores={() => {}}
+//           scrollToBrands={() => {}}
+//           scrollToServices={() => {}}
+//         />
+//         <div
+//           className="text-4xl pb-5 md:text-6xl text-center
+//           bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 mt-24 md:mt-32"
+//         >
+//           Book a meeting
+//         </div>
+
+//         <InlineWidget url="https://calendly.com/socialpulseagencydomain/30min" />
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Book;
