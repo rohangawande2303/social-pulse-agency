55bce249caaf99d8bb147921c7c9dd602fa488f0
Rohan <rohangawande2303@gmail.com>
2024-09-23 18:55:25 +0530

    update in config js

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ["flowbite.s3.amazonaws.com", "images.unsplash.com"],
-  },
-};
-
-export default nextConfig;
