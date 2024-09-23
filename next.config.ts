c0fb18bc69eee84d6d0200aa582c5a3f8cbebf40
Rohan <rohangawande2303@gmail.com>
2024-09-23 18:48:17 +0530

    config file update sitemap

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["flowbite.s3.amazonaws.com", "images.unsplash.com"],
+  },
+};
+
+export default nextConfig;
