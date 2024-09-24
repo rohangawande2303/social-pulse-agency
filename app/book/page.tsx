3129b74cf07e40e8d57f98656654197b27689900
Rohan <rohangawande2303@gmail.com>
2024-09-24 17:16:47 +0530

    book meet page update

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -3,7 +3,7 @@
 import { InlineWidget } from "react-calendly";
 import Navbar from "@/components/navbar";
 
-const Book = () => {
+const BookMeeting = () => {
   return (
     <>
       <div className="flex flex-col w-full h-screen bg-black/[0.96] bg-grid-white/[0.02]">
@@ -14,55 +14,23 @@ const Book = () => {
           scrollToBrands={() => {}}
           scrollToServices={() => {}}
         />
-        <div
-          className="text-4xl pb-5 md:text-6xl text-center 
-          bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 mt-24 md:mt-32"
-        >
-          Book a meeting
-        </div>
+        
+        <header className="text-4xl pb-5 md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 mt-24 md:mt-32">
+          <h1>Book a Meeting</h1>
+        </header>
 
-        <div className="flex-grow flex justify-center items-center p-4">
+        <main className="flex-grow flex justify-center items-center p-4">
           <div className="w-full md:w-3/4 lg:w-2/3" style={{ height: "80vh" }}>
+            <h2 className="sr-only">Schedule a 30-minute consultation</h2>
             <InlineWidget
               url="https://calendly.com/socialpulseagencydomain/30min"
               styles={{ height: "100%" }}
             />
           </div>
-        </div>
+        </main>
       </div>
     </>
   );
 };
 
-export default Book;
-
-// "use client";
-
-// import { InlineWidget } from "react-calendly";
-// import Navbar from "@/components/navbar";
-
-// const Book = () => {
-//   return (
-//     <>
-//       <div className="flex flex-col w-full h-screen bg-black/[0.96] bg-grid-white/[0.02]">
-//         <Navbar
-//           scrollToWebsiteDesign={() => {}}
-//           scrollToGraphicDesign={() => {}}
-//           scrollToShopifyStores={() => {}}
-//           scrollToBrands={() => {}}
-//           scrollToServices={() => {}}
-//         />
-//         <div
-//           className="text-4xl pb-5 md:text-6xl text-center
-//           bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 mt-24 md:mt-32"
-//         >
-//           Book a meeting
-//         </div>
-
-//         <InlineWidget url="https://calendly.com/socialpulseagencydomain/30min" />
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Book;
+export default BookMeeting;
