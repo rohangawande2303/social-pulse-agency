117fb797f4afb4ce407d9865742964a499437743
Rohan <rohangawande2303@gmail.com>
2024-09-24 17:21:10 +0530

    footer and navbar seo update

--- components/footer.tsx
+++ components/footer.tsx
@@ -21,7 +21,7 @@ const Footer = () => {
             </h2>
           </div>
           <div className="text-center sm:text-center">
-            <a href="tel:+918451951123">
+            <a href="tel:+918451951123" aria-label="Call us now">
               <button className="primary-btn border border-white rounded-full py-2 px-4 text-lg hover:bg-white hover:text-black transition-all duration-300 shadow-lg">
                 Call us Now
               </button>
@@ -33,7 +33,7 @@ const Footer = () => {
           <div className="lg:pr-8 text-center md:text-start flex flex-col items-center md:flex-row md:items-center">
             <Image
               src="/logo/logo3.png"
-              alt="WebDev"
+              alt="Social Pulse Agency Logo"
               className="mx-auto pb-4 md:pb-0 md:mx-0"
               width={50}
               height={20}
@@ -49,98 +49,113 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="mx-auto text-center md:text-start sm:text-center">
+          <nav className="mx-auto text-center md:text-start sm:text-center">
             <h3 className="text-xl font-medium mb-6">Quick Links</h3>
             <ul className="list-inside md:list-outside">
               <li className="my-2 text-lg hover:text-gray-300 transition-all duration-300">
-                <Link href="/">Home</Link>
+                <Link href="/" aria-label="Homepage">
+                  Home
+                </Link>
               </li>
               <li className="my-2 text-lg hover:text-gray-300 transition-all duration-300">
-                <Link href="/">About Us</Link>
+                <Link href="/" aria-label="About Us">
+                  About Us
+                </Link>
               </li>
               <li className="my-2 text-lg hover:text-gray-300 transition-all duration-300">
-                <Link href="/contact">Contact us</Link>
+                <Link href="/contact" aria-label="Contact Us">
+                  Contact us
+                </Link>
               </li>
               {/* <li className="my-2 text-lg hover:text-gray-300 transition-all duration-300">
-                <Link href="/portfolio">Portfolio</Link>
+                <Link href="/portfolio" aria-label="Portfolio">Portfolio</Link>
               </li> */}
             </ul>
-          </div>
+          </nav>
 
           <div className="mx-auto text-center md:text-start sm:text-center">
             <h3 className="text-2xl font-medium mb-6">Get In Touch</h3>
-            <ul>
-              <li className="my-2 hover:text-gray-300 transition-all duration-300">
-                <a
-                  className="text-base flex items-center justify-center md:justify-start gap-2"
-                  href="tel:+918451951123"
-                >
-                  <FaPhoneSquareAlt className="text-xl" /> +91-8451951123
-                </a>
-              </li>
-              <li className="my-2 hover:text-gray-300 transition-all duration-300">
-                <a
-                  className="text-base flex items-center justify-center md:justify-start gap-2"
-                  href="mailto:socialpulse23@gmail.com"
-                >
-                  <FaEnvelope className="text-xl" /> socialpulse23@gmail.com
-                </a>
-              </li>
-              <li className="mt-8">
-                <ul className="flex justify-center gap-6 md:gap-6 items-center">
-                  <li>
-                    <a
-                      className="flex items-center justify-center w-10 h-10 text-xl rounded-full border border-white transition-all duration-300 hover:bg-gradient-to-r from-purple-500 to-indigo-500"
-                      href="https://www.facebook.com/profile.php?id=61553120560771&mibextid=ZbWKwL"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaFacebook className="w-5 h-5 text-white" />
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="flex items-center justify-center w-10 h-10 text-xl rounded-full border border-white transition-all duration-300 hover:bg-gradient-to-r from-purple-500 to-indigo-500"
-                      href="https://www.threads.net/@socialpulseagency.in"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaThreads className="w-5 h-5 text-white" />
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="flex items-center justify-center w-10 h-10 text-xl rounded-full border border-white transition-all duration-300 hover:bg-gradient-to-r from-purple-500 to-indigo-500"
-                      href="https://www.instagram.com/socialpulseagency.in/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaInstagram className="w-5 h-5 text-white" />
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="flex items-center justify-center w-10 h-10 text-xl rounded-full border border-white transition-all duration-300 hover:bg-gradient-to-r from-purple-500 to-indigo-500"
-                      href="https://www.linkedin.com/company/social-pulse-agency/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaLinkedinIn className="w-5 h-5 text-white" />
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="flex items-center justify-center w-10 h-10 text-xl rounded-full border border-white transition-all duration-300 hover:bg-gradient-to-r from-purple-500 to-indigo-500"
-                      href="https://www.youtube.com/@SocialPulse23"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaYoutube className="w-5 h-5 text-white" />
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
+            <address>
+              <ul>
+                <li className="my-2 hover:text-gray-300 transition-all duration-300">
+                  <a
+                    className="text-base flex items-center justify-center md:justify-start gap-2"
+                    href="tel:+918451951123"
+                    aria-label="Phone Number: +91-8451951123"
+                  >
+                    <FaPhoneSquareAlt className="text-xl" /> +91-8451951123
+                  </a>
+                </li>
+                <li className="my-2 hover:text-gray-300 transition-all duration-300">
+                  <a
+                    className="text-base flex items-center justify-center md:justify-start gap-2"
+                    href="mailto:socialpulse23@gmail.com"
+                    aria-label="Email: socialpulse23@gmail.com"
+                  >
+                    <FaEnvelope className="text-xl" /> socialpulse23@gmail.com
+                  </a>
+                </li>
+                <li className="mt-8">
+                  <ul className="flex justify-center gap-6 md:gap-6 items-center">
+                    <li>
+                      <a
+                        className="flex items-center justify-center w-10 h-10 text-xl rounded-full border border-white transition-all duration-300 hover:bg-gradient-to-r from-purple-500 to-indigo-500"
+                        href="https://www.facebook.com/profile.php?id=61553120560771&mibextid=ZbWKwL"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="Facebook"
+                      >
+                        <FaFacebook className="w-5 h-5 text-white" />
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="flex items-center justify-center w-10 h-10 text-xl rounded-full border border-white transition-all duration-300 hover:bg-gradient-to-r from-purple-500 to-indigo-500"
+                        href="https://www.threads.net/@socialpulseagency.in"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="Threads"
+                      >
+                        <FaThreads className="w-5 h-5 text-white" />
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="flex items-center justify-center w-10 h-10 text-xl rounded-full border border-white transition-all duration-300 hover:bg-gradient-to-r from-purple-500 to-indigo-500"
+                        href="https://www.instagram.com/socialpulseagency.in/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="Instagram"
+                      >
+                        <FaInstagram className="w-5 h-5 text-white" />
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="flex items-center justify-center w-10 h-10 text-xl rounded-full border border-white transition-all duration-300 hover:bg-gradient-to-r from-purple-500 to-indigo-500"
+                        href="https://www.linkedin.com/company/social-pulse-agency/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="LinkedIn"
+                      >
+                        <FaLinkedinIn className="w-5 h-5 text-white" />
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="flex items-center justify-center w-10 h-10 text-xl rounded-full border border-white transition-all duration-300 hover:bg-gradient-to-r from-purple-500 to-indigo-500"
+                        href="https://www.youtube.com/@SocialPulse23"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="YouTube"
+                      >
+                        <FaYoutube className="w-5 h-5 text-white" />
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </address>
           </div>
         </div>
       </div>
@@ -151,6 +166,7 @@ const Footer = () => {
           <a
             href="#"
             className="hover:text-gray-300 transition-all duration-300"
+            aria-label="Social Pulse Agency Website"
           >
             Socialpulse.in
           </a>{" "}
@@ -162,152 +178,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-// import Link from "next/link";
-// import { FaEnvelope } from "react-icons/fa6";
-// import { FaFacebook } from "react-icons/fa6";
-// // import { FaTwitter } from "@react-icons/all-files/fa/FaTwitter";
-// import { FaThreads } from "react-icons/fa6";
-// import { FaInstagram } from "react-icons/fa6";
-// import { FaLinkedinIn } from "react-icons/fa6";
-// import { FaPhoneSquareAlt } from "react-icons/fa";
-// import Image from "next/image";
-// // import image from "../Images/image";
-
-// const Footer = () => {
-//   return (
-//     <footer className="pt-12 relative bg-black text-white">
-//       <div className="mx-auto max-w-7xl p-6 lg:px-8">
-//         <div className="flex justify-between items-center md:items-start gap-y-8 flex-col sm:flex-row text-center md:text-start border-t border-white pt-6">
-//           <div className="flex-1">
-//             <h2 className="text-2xl md:text-3xl font-medium">
-//               Interested To Get Our Featured Service
-//             </h2>
-//           </div>
-//           <div>
-//             <a href="tel:+918451951123">
-//               <button className="primary-btn shadow-lg">Call us Now</button>
-//             </a>
-//           </div>
-//         </div>
-
-//         <div className="grid grid-cols-1 md:grid-cols-4 gap-y-8 pt-20">
-//           <div className="lg:pr-8 text-center md:text-start">
-//             <Image
-//               src="/logo/logo3.png"
-//               alt="WebDev"
-//               className="mx-auto md:mx-0"
-//               width={80}
-//               height={40}
-//             />
-//             <p className="text-base mt-4 hover:text-gray-300 transition-all duration-300">
-//               We&apos;re Social Pulse, a digital marketing agency fueled by
-//               passion and powered by expertise. We craft magnetic campaigns and
-//               data-driven strategies to amplify your brand voice and ignite
-//               growth.
-//             </p>
-//           </div>
-//           <div className="mx-auto text-center md:text-start">
-//             <div>
-//               <h3 className="text-xl font-medium mb-6">Quick Links</h3>
-//             </div>
-//             <ul className="list-disc list-inside">
-//               <li className="my-2 text-sm hover:text-gray-300 transition-all duration-300">
-//                 <Link href="/">Home</Link>
-//               </li>
-//               <li className="my-2 text-sm hover:text-gray-300 transition-all duration-300">
-//                 <Link href="/about">About Us</Link>
-//               </li>
-//               <li className="my-2 text-sm hover:text-gray-300 transition-all duration-300">
-//                 <Link href="/contact">Contact us</Link>
-//               </li>
-//               {/* <li className="my-2 text-sm hover:text-gray-300 transition-all duration-300">
-//                 <Link href="/portfolio">Portfolio</Link>
-//               </li> */}
-//             </ul>
-//           </div>
-
-//           <div className="mx-auto text-center md:text-start">
-//             <div>
-//               <div className="pb-50 animate get-bottom">
-//                 <h3 className="text-capital text-2xl font-medium mb-6">
-//                   Get In Touch
-//                 </h3>
-//               </div>
-//               <ul>
-//                 <li className="my-2 hover:text-gray-300 transitio   n-all duration-300">
-//                   <a
-//                     className="text-m flex items-center justify-center md:justify-start gap-2"
-//                     href="tel:1234567890"
-//                   >
-//                     <FaPhoneSquareAlt /> +91-8451951123
-//                   </a>
-//                 </li>
-//                 <li className="my-2 hover:text-gray-300 transition-all duration-300">
-//                   <a
-//                     className="text-sm flex items-center justify-center md:justify-start gap-2"
-//                     href="mailto:webdevstudio1@gmail.com"
-//                   >
-//                     <FaEnvelope /> socialpulse23@gmail.com
-//                   </a>
-//                 </li>
-//                 <li className="mt-8">
-//                   <ul className="flex justify-start gap-4 items-center">
-//                     <li className="social">
-//                       <a
-//                         className="social-icon"
-//                         href="https://www.facebook.com/profile.php?id=61553120560771&mibextid=ZbWKwL"
-//                         target="_blank"
-//                         rel="noopener noreferrer"
-//                       >
-//                         <FaFacebook />
-//                       </a>
-//                     </li>
-//                     <li className="social">
-//                       <a
-//                         className="social-icon"
-//                         href="https://www.threads.net/@socialpulseagency.in"
-//                         target="_blank"
-//                         rel="noopener noreferrer"
-//                       >
-//                         <FaThreads />
-//                       </a>
-//                     </li>
-//                     <li className="social">
-//                       <a
-//                         className="social-icon"
-//                         href="https://www.instagram.com/socialpulseagency.in/"
-//                         target="_blank"
-//                         rel="noopener noreferrer"
-//                       >
-//                         <FaInstagram />
-//                       </a>
-//                     </li>
-//                     <li className="social">
-//                       <a
-//                         className="social-icon"
-//                         href="https://www.linkedin.com/company/social-pulse-agency/"
-//                         target="_blank"
-//                         rel="noopener noreferrer"
-//                       >
-//                         <FaLinkedinIn />
-//                       </a>
-//                     </li>
-//                   </ul>
-//                 </li>
-//               </ul>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-
-//       <div className="text-center py-6 mt-4 md:mt-8 lg:mt-12 mb-[4.5rem] md:mb-0 border-t border-white pt-6">
-//         <p className="text-sm">
-//           © Copyright <a href="#">Socialpulse.in</a> 2024. All rights reserved.
-//         </p>
-//       </div>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -52,20 +52,27 @@ const Navbar = ({
 
   return (
     <>
-      <div className="fixed top-0 w-full z-50 bg-black">
+      <nav
+        className="fixed top-0 w-full z-50 bg-black"
+        aria-label="Main Navigation"
+      >
         <div className="p-4 md:p-6 lg:px-8 flex items-center justify-between md:justify-around">
           <div className="flex items-center">
-            <Link className="cursor-pointer" href="/">
+            <Link className="cursor-pointer" href="/" aria-label="Homepage">
               <Image
                 priority
                 src="/logo/logo3.png"
-                alt="Logo"
+                alt="Social Pulse Agency Logo"
                 width={90}
                 height={90}
                 className="w-auto h-10 md:w-auto md:h-16"
               />
             </Link>
-            <Link href="/" className="text-white text-xl md:text-2xl ml-3">
+            <Link
+              href="/"
+              className="text-white text-xl md:text-2xl ml-3"
+              aria-label="Visit Social Pulse Agency"
+            >
               Social Pulse Agency
             </Link>
           </div>
@@ -77,22 +84,28 @@ const Navbar = ({
                bg-gradient-to-b from-neutral-50
                 to bg-neutral-400 bg-opacity-50 text-lg"
           >
-            <Link href="/" className="hover:text-gray-50">
+            <Link href="/" className="hover:text-gray-50" aria-label="Home">
               Home
             </Link>
             <div
               onClick={() => handleScrollOrRedirect(scrollToGraphicDesign)}
-              className="hover:text-gray-50"
+              className="hover:text-gray-50 cursor-pointer"
+              aria-label="About Us"
             >
               About Us
             </div>
             <div
               onClick={() => handleScrollOrRedirect(scrollToBrands)}
-              className="hover:text-gray-50"
+              className="hover:text-gray-50 cursor-pointer"
+              aria-label="Services"
             >
               Services
             </div>
-            <Link href="/contact" className="hover:text-gray-50">
+            <Link
+              href="/contact"
+              className="hover:text-gray-50"
+              aria-label="Contact Us"
+            >
               Contact
             </Link>
           </div>
@@ -129,134 +142,16 @@ const Navbar = ({
                focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2
                 focus:ring-offset-slate-50
               "
+              aria-label="Contact us via WhatsApp"
             >
               What&apos;s App
             </a>
           </div>
         </div>
-      </div>
+      </nav>
       <div className="pt-20 md:pt-24">{/* Main content starts here */}</div>
     </>
   );
 };
 
 export default Navbar;
-
-// "use client";
-// import { useState } from "react";
-// import { motion } from "framer-motion";
-// import { AlignJustify, X } from "lucide-react";
-
-// import Image from "next/image";
-// import Link from "next/link";
-// import DropDownMenu from "./drop-down-menu";
-
-// interface NavbarProps {
-//   scrollToWebsiteDesign: () => void;
-//   scrollToGraphicDesign: () => void;
-//   scrollToShopifyStores: () => void;
-//   scrollToBrands: () => void;
-//   scrollToServices: () => void; // Define scrollToServices function
-// }
-
-// const Navbar = ({
-//   scrollToWebsiteDesign,
-//   scrollToGraphicDesign,
-//   scrollToShopifyStores,
-//   scrollToBrands,
-//   scrollToServices, // Add scrollToServices to props
-// }: NavbarProps) => {
-//   const [isDropDownVisible, setIsDropDownVisible] = useState(false);
-
-//   const toggleDropDown = () => {
-//     setIsDropDownVisible(!isDropDownVisible);
-//   };
-
-//   const closeDropDown = () => {
-//     setIsDropDownVisible(false);
-//   };
-
-//   return (
-//     <div>
-//       <div className="p-6 md:p-10 flex items-center justify-between z-50">
-//         <div>
-//           <Link className="cursor-pointer" href="/">
-//             <Image
-//               priority
-//               src="/logo/logo3.png"
-//               alt="Logo"
-//               width={100}
-//               height={100}
-//               className="w-10 h-10 md:w-14 md:h-14"
-//             />
-//           </Link>
-//         </div>
-//         <div
-//           className="cursor-pointer hidden
-//             md:flex space-x-10 items-center
-//              text-slate-300 text-center
-//              bg-clip-text text-transparent
-//              bg-gradient-to-b from-neutral-50
-//               to bg-neutral-400 bg-opacity-50"
-//         >
-//           <div onClick={scrollToWebsiteDesign} className="hover:text-gray-50">
-//             Home
-//           </div>
-//           <div onClick={scrollToGraphicDesign} className="hover:text-gray-50">
-//             About Us
-//           </div>
-
-//           {/* <div onClick={scrollToShopifyStores} className="hover:text-gray-50">
-//             Shopify Stores
-//           </div> */}
-//           <div onClick={scrollToBrands} className="hover:text-gray-50">
-//             Testimonials
-//           </div>
-
-//           <Link href="/contact" className="hover:text-gray-50">
-//             Contact
-//           </Link>
-//         </div>
-
-//         <div className="flex md:hidden">
-//           {isDropDownVisible ? (
-//             // display an x icon when the drop is visible
-//             <div
-//               onClick={toggleDropDown}
-//               className="w-8 h-8 text-slate-300 cursor-pointer"
-//             >
-//               <X />
-//               <DropDownMenu
-//                 onClose={closeDropDown}
-//                 scrollToServices={scrollToServices} // Pass scrollToServices
-//               />
-//             </div>
-//           ) : (
-//             <AlignJustify
-//               onClick={toggleDropDown}
-//               className="w-8 h-8 text-slate-300 cursor-pointer"
-//             />
-//           )}
-//         </div>
-
-//         <div className="hidden md:flex">
-//           <Link
-//             href="/contact"
-//             className="
-//             inline-flex h-12 animate-shimmer items-center justify-center
-//             rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)]
-//             bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors
-//              focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2
-//               focus:ring-offset-slate-50
-
-//             "
-//           >
-//             Contact
-//           </Link>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
