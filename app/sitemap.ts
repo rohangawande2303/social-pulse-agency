9f53235ad040d3e4a25c2747a6b17a5c2c42adbe
Rohan <rohangawande2303@gmail.com>
2025-12-01 16:42:52 +0530

    added founder page in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -20,6 +20,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.8,
     },
+    {
+      url: "https://www.socialpulseagency.in/rishabhmishra",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
     // Services Pages
     {
       url: "https://www.socialpulseagency.in/services/web-development",
